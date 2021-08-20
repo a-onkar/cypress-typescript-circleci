52af2e861b25a1f41b97f32efe8983ac0245946b
Anandteerth Onkar <mailanandteerth@gmail.com>
2021-08-20 14:15:04 +0100

    added web elements to page class & updated baseURL

--- cypress/pageObjects/landingpage.page.ts
+++ cypress/pageObjects/landingpage.page.ts
@@ -1,8 +1,17 @@
 export default class LandingPage {
-  private pageTitle: string =
-    'what3words /// The simplest way to talk about location';
+  private pageTitle = 'what3words /// The simplest way to talk about location';
+  private searchBox = '[data-testid="SearchPanel-Input"]';
+  private headerMenu = '[data-testid="HeaderMenu-Menu_open"]';
 
   verifyPageTitle(): void {
     cy.title().should('eq', this.pageTitle);
+  }
+
+  getSearchBox(): Cypress.Chainable<JQuery<HTMLElement>> {
+    return cy.get(this.searchBox).should('be.visible');
+  }
+
+  getHeaderMenuButton(): Cypress.Chainable<JQuery<HTMLElement>> {
+    return cy.get(this.headerMenu).should('be.visible');
   }
 }
