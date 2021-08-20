6aa63e367cb6ad810c90ee1df893ccec8096ba32
Anandteerth Onkar <mailanandteerth@gmail.com>
2021-08-20 20:25:00 +0100

    added function to handle copy clipboard functionality

--- cypress/integration/task2.spec.ts
+++ cypress/integration/task2.spec.ts
@@ -14,5 +14,26 @@ describe('Task 2: Share - action tab functionality testing using Cypress', () =>
     landPageObj.getSearchBox().type(testdata.address1);
     landPageObj.selectAddressFromSearchResult(testdata.address1);
     actionsObj.getShareButton().click();
+
+    // Setting permission in browser to read cpoied text
+    cy.wrap(
+      Cypress.automation('remote:debugger:protocol', {
+        command: 'Browser.grantPermissions',
+        params: {
+          permissions: ['clipboardReadWrite', 'clipboardSanitizedWrite'],
+          origin: window.location.origin,
+        },
+      })
+    );
+    actionsObj.getCopyButton().click();
+    cy.window()
+      .its('navigator.permissions')
+      .invoke('query', { name: 'clipboard-read' })
+      .its('state')
+      .then(cy.log);
+    cy.window()
+      .its('navigator.clipboard')
+      .invoke('readText')
+      .should('equal', 'https://w3w.co/certified.potato.vine');
   });
 });
